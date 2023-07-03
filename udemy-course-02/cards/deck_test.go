f54ac42be062b2a3a2c9e30d18950ee02a6c08e4
Karlo Jakobović <k.jakobovic@pan-net.eu>
2023-07-03 18:44:06 +0200

    practice

---
+++ udemy-course-02/cards/deck_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+// t.Errorf("Expected deck length of 16, but got", len(d))
+// t.Errorf("Expected deck length of 16, but got %v", len(d))
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 52 {
+		t.Errorf("The lenght of a new deck is less than 52!\tGot: %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Want: Ace of Spades\tGot: %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Want: King of Clubs\tGot: %v", d[len(d)-1])
+	}
+}
